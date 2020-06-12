811b99caa85ea2ea78abba5e7acbc1a83b233e08
sgong-pdftron <sgong@pdftron.com>
2020-06-12 12:31:43 -0700

    Update collaboration sample to match latest Android SDK

--- CollaborationAndroid/MainActivity.cs
+++ CollaborationAndroid/MainActivity.cs
@@ -1,35 +1,26 @@
 ﻿using System;
+
 using Android.App;
 using Android.OS;
 using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
-using Android.Arch.Lifecycle;
-
-using pdftron.PDF.Controls;
+using pdftron.Collab.UI.Viewer;
+using pdftron.Collab.WebViewerServer;
 using pdftron.PDF.Config;
 
-using pdftron.Collab.UI.Viewer;
-using pdftron.Collab.ViewModel;
-using pdftron.Collab.WebViewerServer;
-
-
-namespace CollaborationSample
+namespace CollaborationAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/CustomAppTheme", MainLauncher = true,
         WindowSoftInputMode = SoftInput.AdjustPan,
         ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.KeyboardHidden)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AndroidX.AppCompat.App.AppCompatActivity
     {
 
         CollabViewerTabHostFragment mPdfViewCtrlTabHostFragment;
 
-        // To start collaboration, go to
-        // https://www.pdftron.com/webviewer/demo/document-collaboration?shareId=8v7y2IgzUELQ&cId=https%3A%2F%2Fpdftron.s3.amazonaws.com%2Fdownloads%2Fpl%2FReport_2011.pdf
-        // or open this sample on different devices
+        // To start collaboration, open this sample on two different devices
         string DEFAULT_SHARE_ID = "8v7y2IgzUELQ";
         string DEFAULT_FILE_URL = "https://pdftron.s3.amazonaws.com/downloads/pl/Report_2011.pdf";
         string WVS_ROOT = "https://demo.pdftron.com/";
@@ -39,11 +30,11 @@ namespace CollaborationSample
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            pdftron.PDF.Tools.Utils.AppUtils.InitializePDFNetApplication(this);
             SetContentView(Resource.Layout.activity_main);
 
-            mBlackBoxConnection = new BlackBoxConnection(this.ApplicationContext);
-            
+            mBlackBoxConnection = new BlackBoxConnection();
+
             var fileUri = Android.Net.Uri.Parse(DEFAULT_FILE_URL);
             StartTabHostFragment(fileUri, "");
         }
@@ -88,7 +79,7 @@ namespace CollaborationSample
                 return;
             }
 
-            Android.Support.V4.App.FragmentTransaction ft = this.SupportFragmentManager.BeginTransaction();
+            var ft = this.SupportFragmentManager.BeginTransaction();
             CreateViewerFragment(fileUri);
 
             mPdfViewCtrlTabHostFragment.NavButtonPressed += (sender, e) =>
@@ -110,17 +101,13 @@ namespace CollaborationSample
 
         private void HandleTabDocumentLoaded(String tag)
         {
-            DocumentViewModel documentViewModel = (DocumentViewModel) ViewModelProviders.Of(this).Get(Java.Lang.Class.FromType(typeof(DocumentViewModel)));
-            documentViewModel.SetCustomConnection(mBlackBoxConnection);
+            mBlackBoxConnection.SetCollabManager(mPdfViewCtrlTabHostFragment.CollabManager);
             mBlackBoxConnection.Start(WVS_ROOT, DEFAULT_FILE_URL, DEFAULT_SHARE_ID);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+    }
 }
-
