e0ca9b351b3abd22dde4f8bcb29027c3ff4b1c23
Dominic Cupidon <dcupidon@pdftron.com>
2021-10-15 17:26:51 -0700

    Added .NET 6 iOS sample and refactored the folder structure

---
+++ Net6/Net6iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using pdftron;
+using pdftron.PDF.Controls;
+
+namespace Net6iOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow? Window
+    {
+        get;
+        set;
+    }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        // adding the document controller to a navigation controller
+        // this is needed so the document controller's navigation toolbar renders
+        var documentController = new PTDocumentController();
+        var navigationController = new UINavigationController(documentController);
+
+        // attach navigation controller to window
+        Window.RootViewController = navigationController;
+
+        // make the window visible
+        Window.MakeKeyAndVisible();
+
+        // open the document
+        documentController.OpenDocumentWithURL(new Uri("https://pdftron.s3.amazonaws.com/downloads/pdfref.pdf"));
+
+        return true;
+    }
+}
+
---
+++ Net6/README.md
@@ -0,0 +1,34 @@
+# About
+This sample shows how to use the PDFTron mobile SDK with .NET 6.0 release candidate 1 
+
+**Note:** Official support for the SDK is being implemented.
+
+# Prerequesites
+
+* [.NET 6 SDK Release Candidate 1](https://dotnet.microsoft.com/download/dotnet/6.0)
+* [Visual Studio](https://visualstudio.microsoft.com/downloads/) 
+    * Option 1: 2022 Release Candidate or 2022 for Mac Preview 
+    * Option 2: 2019 v16.11 or 2019 for Mac v8.10
+* [Android 12 SDK](https://developer.android.com/about/versions/12/setup-sdk) 
+* [XCode 13](https://developer.apple.com/xcode/) for iOS
+
+# Instructions
+
+## .NET 6 for Android
+
+1. Open the [`Net6Android`](./net6/Net6Android) project in Visual Studio.
+2. Restore the packages.
+3. Run the project.
+
+## .NET 6 for iOS
+
+1. Open the [`Net6iOS`](./net6/Net6iOS) project in Visual Studio.
+2. Restore the packages.
+3. Run the project.
+
+## .NET MAUI
+
+1. Open the [`Net6MAUI`](./net6/Net6MAUI) project in Visual Studio.
+2. Restore the packages.
+3. Choose the platform that you want to run on.
+4. Run the project.
